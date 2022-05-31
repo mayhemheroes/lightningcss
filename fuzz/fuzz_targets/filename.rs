2ea7a351c84a60ed8228601a3e237af492eeba35
Matthew Jurenka <matt.jurenka@comcast.net>
2022-05-31 00:23:21 -0700

    initialized mayhem fuzz github action

---
+++ fuzz/fuzz_targets/filename.rs
@@ -0,0 +1,35 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    match std::str::from_utf8(data) {
+        Ok(s) => {
+            use parcel_css::stylesheet::{
+                StyleSheet, ParserOptions, MinifyOptions, PrinterOptions
+            };
+
+            // Parse a style sheet from a string.
+            match StyleSheet::parse(
+                s,
+                r#"
+                .foo {
+                  color: red;
+                }
+
+                .bar {
+                  color: red;
+                }
+                "#,
+                ParserOptions::default()
+            ) {
+                Ok(mut stylesheet) => {
+                    stylesheet.minify(MinifyOptions::default()).unwrap();
+                    stylesheet.to_css(PrinterOptions::default()).unwrap();
+                },
+                _ => {
+                },
+            };
+        },
+        _ => {},
+    }
+});
